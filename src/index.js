a2a396006f24aa8af4aa9c5d18e5f645ecb6f5db
ZJP <2712104231@qq.com>
2025-01-09 11:13:04 +0800

    更新目录

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import openapi from './openapi.json'
-import generatornjk from './generator.njk.js';
 import {
   apiGenerator
 } from './generator'
-export function APIGenerator(apiTemplate = generatornjk, openapi, genType = 'ts') {
+export function APIGenerator(apiTemplate, openapi, genType = 'ts') {
   return apiGenerator(apiTemplate, openapi, genType)
 }
-
-APIGenerator(generatornjk, openapi, 'ts')
